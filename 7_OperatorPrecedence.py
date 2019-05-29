f5855090c6fe6bc9d62948eb357fe20a4006e993
Rajesh Kumar <rajarya.java@gmail.com>
2019-05-30 02:09:08 +0530

    Operator precedence

---
+++ 7_OperatorPrecedence.py
@@ -0,0 +1,11 @@
+# order of operations
+# exponentiation **
+# multiplication or division
+# addition or subtraction
+
+result = 10 - 2 * 3 ** 2
+print(result)
+
+# Parenthesis change priority of execution
+result = (10 - 2) * 3 ** 2
+print(result)
