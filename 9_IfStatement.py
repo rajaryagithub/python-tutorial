2559a341dfef05318dc447dcdae89457ffb38e1f
Rajesh Kumar <rajarya.java@gmail.com>
2019-05-30 02:31:52 +0530

    If statements

---
+++ 9_IfStatement.py
@@ -0,0 +1,24 @@
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's a hot day")
+    print('Drink plenty of water')
+elif is_cold:
+    print("It's a cold day")
+    print('Wear warm cloths')
+else:
+    print("It's a lovely day")
+
+print('Enjoy your day')
+
+# Solution
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+
+print(f"Down payment is ${down_payment}")
