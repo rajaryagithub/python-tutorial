591b997b3834f37c221d918a2957565b27b5a98b
Rajesh Kumar <rajarya.java@gmail.com>
2019-05-30 01:53:47 +0530

    Arithmetic operators

---
+++ 6_ArithmeticOperations.py
@@ -0,0 +1,13 @@
+# Operators
+print(10 + 3)
+
+# Two types of division operators
+print(10 / 3)  # returns float
+print(10 // 3)  # returns integer
+print(10 % 3)  # returns remainder
+print(10 ** 3)  # returns power, exponent
+
+# Augmented/enhanced assignment operator
+value = 10
+value += 5
+print(value)
