65a228edb9bf2079e7814d8f154c043e90de9180
Rajesh Kumar <rajarya.java@gmail.com>
2019-05-30 02:17:54 +0530

    Math functions

---
+++ 8_MathFunctions.py
@@ -0,0 +1,11 @@
+float_value = 2.9
+print(round(float_value))
+
+minus_value = -37
+print(abs(minus_value))
+
+# Module in python is a separate file with some reusable code
+import math
+
+print(math.ceil(2.4))
+print(math.floor(2.9))
